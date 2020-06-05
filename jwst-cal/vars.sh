90711601bf61247e9dd2eb6cd117d0ec26d72e8c
EC2 Default User <ec2-user@ip-172-31-89-85.ec2.internal>
2020-06-05 14:02:32 +0000

    Corrected jwst-cal repo in vars.sh.   Switched to Python 3.7.7

--- jwst-cal/vars.sh
+++ jwst-cal/vars.sh
@@ -1,6 +1,6 @@
 export PROJECT=${5:-"jwst-cal"}
 export VERSION=${1:-`cat ${PROJECT}/VERSION`}
-export REPO=${2:-"jwst-cal-user-image"}
+export REPO=${2:-"jwst-cal"}
 export ACCOUNT=${3:-"162808325377.dkr.ecr.us-east-1.amazonaws.com"}
 export REGION=${4:-"us-east-1"}
 
