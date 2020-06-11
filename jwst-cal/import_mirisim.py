d4b1470cfb47d25095f8b25a0b1f37f0d3d8e319
Todd Miller <jmiller@stsci.edu>
2020-06-11 16:10:53 +0000

    Moved jwst-cal/0.13.7/* to jwst-cal. Updated jwst-cal to jwstdp-0.16.2 (DMS Build 7.5 as-of now) Updated jwst-cal with MIRISIM 2.2.0, and MIRAGE 1.3.3 ( Added import checks for jwst-cal, mirisim, mirage. Updated CAL .cfg files. CAL notebook runs locally with CRDS env changes,  data may have issues with expected units. Added utils directory with login, build, run, push scripts driven by vars.sh files. Added an extra digit to jwst-cal VERSION to supplement the normal CAL release tag.

---
+++ jwst-cal/import_mirisim.py
@@ -0,0 +1,9 @@
+
+print("-"*30, "mirisim check", "-"*30)
+
+import miri
+print( "miri.__version__ =", miri.__version__ )
+import mirisim
+print( "mirisim.__version__ = ", mirisim.version.__version__ )
+
+print("-"*70)
